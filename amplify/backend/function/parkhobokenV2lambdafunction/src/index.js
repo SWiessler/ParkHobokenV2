604e3e12dd9aa8df781de7629d03a4da9970d1e2
WiesslerS <s.wiessler@comcast.net>
2022-03-07 14:20:57 -0500

    Working amplify & dynamo

--- amplify/backend/function/parkhobokenV2lambdafunction/src/index.js
+++ amplify/backend/function/parkhobokenV2lambdafunction/src/index.js
@@ -6,41 +6,36 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-var aws = require('aws-sdk');
-var ddb = new aws.DynamoDB();
+var aws = require('aws-sdk')
+var ddb = new aws.DynamoDB()
 
 exports.handler = async (event, context) => {
-    
-    let date = new Date();
-
+    let date = new Date()
     if (event.request.userAttributes.sub) {
-
         let params = {
             Item: {
                 'id': {S: event.request.userAttributes.sub},
                 '__typename': {S: 'User'},
+                'username': {S: event.userName},
                 'email': {S: event.request.userAttributes.email},
-                'departing': {S: false},
                 'createdAt': {S: date.toISOString()},
                 'updatedAt': {S: date.toISOString()},
             },
-            TableName: process.env.API_{YOUR_APP_NAME}_USERTABLE_NAME
-        };
-
-        // Call DynamoDB
-        try {
-            await ddb.putItem(params).promise()
-            console.log("Success");
-        } catch (err) {
-            console.log("Error", err);
+            TableName: process.env.USERTABLE
         }
 
-        console.log("Success: Everything executed correctly");
-        context.done(null, event);
+        try {
+            await ddb.putItem(params).promise()
+            console.log("Success")
+        } catch (err) {
+            console.log("Error", err)
+        }
+
+        console.log("Success: Everything executed correctly")
+        context.done(null, event)
 
     } else {
-        // Nothing to do, the user's email ID is unknown
-        console.log("Error: Nothing was written to DynamoDB");
-        context.done(null, event);
+        console.log("Error: Nothing was written to DynamoDB")
+        context.done(null, event)
     }
 };
