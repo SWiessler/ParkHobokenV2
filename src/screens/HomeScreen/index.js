663957410bddeb05cf6be1fb34b5ec32ada32330
WiesslerS <s.wiessler@comcast.net>
2022-03-28 18:05:18 -0400

    Value change in dynamodb works

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -5,14 +5,34 @@ import HomeMap from '../../components/HomeMap';
 // import CovidMessage from '../../components/CovidMessage';
 import HomeSearch from '../../components/HomeSearch';
 import CustomDrawer from "../../navigation/CustomDrawer";
-import updateUser from "../../graphql/mutations";
+import * as mutations from "../../graphql/mutations";
+import * as queries from "../../graphql/queries";
 import Auth from '@aws-amplify/auth';
+import { API, Amplify } from "aws-amplify";
+import config from "../../aws-exports";
+
+Amplify.configure(config);
 
 const HomeScreen = (props) => {
   // Auth.currentAuthenticatedUser().then((user) => {
   //   console.log('user email = ' + user.attributes.email);
   // });
 
+  async function getUserEmail () {
+    try{
+      const user = await Auth.currentUserInfo();
+      return user.attributes.email;
+    }catch(e){
+      console.log(e);
+    }
+
+  }
+// shows getting email outside of func
+  // async function printEmail () {
+  //   console.log( await getUserEmail());
+  // }
+
+  // printEmail();
   
   return (
     <View>
@@ -27,13 +47,31 @@ const HomeScreen = (props) => {
       <Button
       title="I'm Departing!"
       color="#800000"
-      onPress={Auth.currentAuthenticatedUser().then((user) => updateUser(
-        {
-          email: user.attributes.email, 
-          departing: 'true',
+      onPress={async () => {
+        const user = await Auth.currentUserInfo();
+        console.log(user);
+        
+        // console.log(JSON.stringify(updateUserDetails));
+
+        const updateUserDetails = {
+          id: user.username,
+          username: user.username,
+          email: user.attributes.email,
+          departing: "true"
+        };
+
+        try{
+          const userUpdate = await API.graphql({
+            query: mutations.updateUser,
+            variables: {input: updateUserDetails}
+            // authMode: 'AWS_IAM'
+          });
+          console.log(JSON.stringify(userUpdate));
+        }catch(e){
+          console.log(e);
         }
-      ))
-    }
+
+      }}
       />
     
     </View>
